6855ea7e97bfeacde08214585f098a607d97197d
DmitryBohan <zorrorul@gmail.com>
2024-05-23 11:05:38 +0300

    добавил метода создания счета , зачисленя депозита , снятия средств с счета . создал тестовый счет , где попробовал нормальную работу программы . Создал тестовые возбуждения исключений

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,6 +2,8 @@ public class Account {
 
      double balance;
 
+    /**
+     * конструктор создания счета с проверкой  */
     public Account(double initialBalance) throws IllegalArgumentException {
         if (initialBalance < 0) {
             throw new IllegalArgumentException("Начальный баланс не может быть отрицательным ");
@@ -9,7 +11,27 @@ public class Account {
         this.balance = initialBalance;
     }
 
+    /**
+     * метод внесения депозита с проверкой на отрицательное значение
+     */
+    public void deposit(double amount) throws IllegalArgumentException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Сумма депозита не может быть отрицательной.");
+        }
+        balance += amount;
+    }
 
+    /**
+     * метод вывода средств с счета с проверкой на отрицатльный остаток
+     * */
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount > balance) {
+            throw new InsufficientFundsException("Недостаточно средств. Текущий баланс: " + balance);
+        }
+        balance -= amount;
+    }
 
-
+    public double getBalance() {
+        return balance;
+    }
 }
